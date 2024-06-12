f89c6cdb7b8a678526e2bc5798882f999eef6459
wanglei <wllenyj@gmail.com>
2024-06-12 16:53:37 +0800

    use LazyVim
    Signed-off-by: wanglei <wllenyj@gmail.com>

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -1,22 +1,2 @@
-require("user.plugins")
-
-require("lazy").setup({
-    "neovim/nvim-lspconfig",
-    "tjdevries/lsp_extensions.nvim",
-    
-    "hrsh7th/nvim-cmp",
-    "hrsh7th/cmp-nvim-lsp",
-    
-    "dcampos/nvim-snippy",
-    "dcampos/cmp-snippy",
-    
-    "jlanzarotta/bufexplorer",
-    "dhananjaylatkar/cscope_maps.nvim",
-
-    "vim-airline/vim-airline",
-})
-
-
-require("user.lsp")
-require("user.cmp")
-
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
---
+++ nvim/lua/plugins/utils.lua
@@ -0,0 +1,5 @@
+return {
+    "vim-airline/vim-airline",
+    "jlanzarotta/bufexplorer",
+    "dhananjaylatkar/cscope_maps.nvim",
+}
