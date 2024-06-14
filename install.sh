eefe72cd511c89e85f2508e2d0c10e040052c13e
wanglei <wllenyj@gmail.com>
2024-06-14 16:31:47 +0800

    update install script
    Signed-off-by: wanglei <wllenyj@gmail.com>

--- install.sh
+++ install.sh
@@ -8,31 +8,32 @@ HERE=$(readlink -f $0)
 CUR_PATH=$(cd $(dirname $HERE) && pwd)
 ((DEBUG)) && echo $CUR_PATH
 
-grep "bash/prompt.sh" $HOME/.bashrc >> /dev/null
+grep "bash/prompt.sh" $HOME/.bashrc >>/dev/null
 if [[ $? != 0 ]]; then
-    echo -e "\nsource $CUR_PATH/bash/prompt.sh" >> $HOME/.bashrc
+	echo -e "\nsource $CUR_PATH/bash/prompt.sh" >>$HOME/.bashrc
 fi
 
 source $HOME/.bashrc
 
 if [[ ! -d $HOME/.vim ]]; then
-    ln -s $CUR_PATH/vim $HOME/.vim
+	ln -s $CUR_PATH/vim $HOME/.vim
 fi
 
 if [[ ! -d $HOME/.screenrc ]]; then
-    ln -s $CUR_PATH/bash/screenrc $HOME/.screenrc
+	ln -s $CUR_PATH/bash/screenrc $HOME/.screenrc
 fi
 
-if [[ ! -d $HOME/.tmux.conf ]]; then
-    ln -s $CUR_PATH/bash/tmux.conf $HOME/.tmux.conf
+if [[ ! -d $HOME/.config/tmux ]]; then
+	ln -s $CUR_PATH/tmux $HOME/.config/tmux
+else
+	echo 'exist .config/tmux dir'
 fi
 
 if [[ ! -d $HOME/.config/nvim ]]; then
-    echo 'not exist .config/nvim dir'
-    if [[ ! -d $HOME/.config/nvim/init.vim ]]; then
-        ln -s $CUR_PATH/nvim/init.vim $HOME/.config/nvim/init.vim
-    fi
+	echo 'not exist .config/nvim dir'
+	if [[ ! -d $HOME/.config/nvim/init.vim ]]; then
+		ln -s $CUR_PATH/nvim/init.vim $HOME/.config/nvim/init.vim
+	fi
 else
-    echo 'exist .config/nvim dir'
+	echo 'exist .config/nvim dir'
 fi
-
