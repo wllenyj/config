72e6c45f620a60a7a64086f6f5a29c3bbe13cf0b
wanglei <wllenyj@gmail.com>
2024-06-15 21:24:01 +0800

    nvim: disable keybinds in telescope preview
    Co-authored-by: wanglei <wllenyj@gmail.com>

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,34 +2,35 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     keys = {
-      {
-        "<c-k",
-        function()
-          require("telescope.actions").preview_scrolling_up(0)
-        end,
-        desc = "socrolling up preview",
-      },
-      {
-        "<c-j",
-        function()
-          require("telescope.actions").preview_scrolling_down(0)
-        end,
-        desc = "socrolling down preview",
-      },
-      {
-        "<c-h",
-        function()
-          require("telescope.actions").preview_scrolling_left(0)
-        end,
-        desc = "socrolling left preview",
-      },
-      {
-        "<c-l",
-        function()
-          require("telescope.actions").preview_scrolling_right(0)
-        end,
-        desc = "socrolling right preview",
-      },
+      --  {
+      --    "<c-k",
+      --    function()
+      --      require("telescope.actions").preview_scrolling_up(0)
+      --    end,
+      --    desc = "socrolling up preview",
+      --  },
+      --  {
+      --    "<c-j",
+      --    function()
+      --      require("telescope.actions").preview_scrolling_down(0)
+      --    end,
+      --    desc = "socrolling down preview",
+      --  },
+      --{
+      --  "<c-h",
+      --  function()
+      --    require("telescope.actions").preview_scrolling_left(0)
+      --  end,
+      --  mode = "i",
+      --  desc = "socrolling left preview",
+      --},
+      --  {
+      --    "<c-l",
+      --    function()
+      --      require("telescope.actions").preview_scrolling_right(0)
+      --    end,
+      --    desc = "socrolling right preview",
+      --  },
       {
         "<leader><leader>",
         function()
