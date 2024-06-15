3165aa4daa6e1e13ea8b54040b4b3f84ba275a5c
wanglei <wllenyj@gmail.com>
2024-06-15 21:04:11 +0800

    nivm: add description for bufferline keybinds
    Co-authored-by: wanglei <wllenyj@gmail.com>

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -20,60 +20,70 @@ return {
         function()
           require("bufferline.commands").go_to(1, true)
         end,
+        desc = "Go to 1 th tab",
       },
       {
         "<localleader>2",
         function()
           require("bufferline.commands").go_to(2, true)
         end,
+        desc = "Go to 2 th tab",
       },
       {
         "<localleader>3",
         function()
           require("bufferline.commands").go_to(3, true)
         end,
+        desc = "Go to 3 th tab",
       },
       {
         "<localleader>4",
         function()
           require("bufferline.commands").go_to(4, true)
         end,
+        desc = "Go to 4 th tab",
       },
       {
         "<localleader>5",
         function()
           require("bufferline.commands").go_to(5, true)
         end,
+        desc = "Go to 5 th tab",
       },
       {
         "<localleader>6",
         function()
           require("bufferline.commands").go_to(6, true)
         end,
+        desc = "Go to 6 th tab",
       },
       {
         "<localleader>7",
         function()
           require("bufferline.commands").go_to(7, true)
         end,
+        desc = "Go to 7 th tab",
       },
       {
         "<localleader>8",
         function()
           require("bufferline.commands").go_to(8, true)
         end,
+        desc = "Go to 8 th tab",
       },
       {
         "<localleader>9",
         function()
           require("bufferline.commands").go_to(9, true)
         end,
+        desc = "Go to 9 th tab",
       },
       {
         "<localleader>0",
         function()
           require("bufferline.commands").go_to(-1, true)
         end,
+        desc = "Go to last tab",
       },
     },
   },
