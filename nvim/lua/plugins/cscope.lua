2ec995a0cc98617c4a20e848cd2f23d78d3ad11a
wanglei <wllenyj@gmail.com>
2024-06-15 21:07:31 +0800

    nvim: lua format
    Co-authored-by: wanglei <wllenyj@gmail.com>

--- nvim/lua/plugins/cscope.lua
+++ nvim/lua/plugins/cscope.lua
@@ -1,20 +1,20 @@
 return {
-    {
-        "dhananjaylatkar/cscope_maps.nvim",
-        dependencies = {
-            "nvim-telescope/telescope.nvim",
-        },
-        config = function(_, opts)
-            require("cscope_maps").setup(opts)
-        end,
-        opts = {
-            prefix = "<leader>g",
-            skip_input_prompt = true,
-            cscope = {
-                picker = "telescope",
-                skip_picker_for_single_result = true,
-                db_build_cmd_args = { "-bqkv" },
-            }
-        }
+  {
+    "dhananjaylatkar/cscope_maps.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
     },
+    config = function(_, opts)
+      require("cscope_maps").setup(opts)
+    end,
+    opts = {
+      prefix = "<localleader>g",
+      skip_input_prompt = true,
+      cscope = {
+        picker = "telescope",
+        skip_picker_for_single_result = true,
+        db_build_cmd_args = { "-bqkv" },
+      },
+    },
+  },
 }
