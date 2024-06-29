29ba9d305547ccb20a2234d6c3c9d02053f36747
wanglei <wllenyj@gmail.com>
2024-06-29 22:22:43 +0800

    nvim: add rust inlay hints
    Signed-off-by: wanglei <wllenyj@gmail.com>

---
+++ nvim/lua/plugins/rust.lua
@@ -0,0 +1,28 @@
+return {
+  "mrcjkb/rustaceanvim",
+  opts = {
+    server = {
+      default_settings = {
+        ["rust-analyzer"] = {
+          inlayHints = {
+            typeHints = {
+              enable = true,
+            },
+            lifetimeElisionHints = {
+              enable = true,
+              useParameterNames = false,
+            },
+            expressionAdjustmentHints = { enable = true },
+            chainingHints = { enable = true },
+            parameterHints = { enable = true },
+            bindingModeHints = { enable = true },
+            closureCaptureHints = { enable = true },
+            closureReturnTypeHints = { enable = true },
+            discriminantHints = { enable = true },
+            implicitDrops = { enable = true },
+          },
+        },
+      },
+    },
+  },
+}
