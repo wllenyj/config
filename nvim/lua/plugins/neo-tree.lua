ab4f67d3b0881165422375a18cc7c7c16b26fd69
wanglei <wllenyj@gmail.com>
2024-07-22 23:56:28 +0800

    nvim: neo-tree remap keys
    Signed-off-by: wanglei <wllenyj@gmail.com>

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -19,5 +19,24 @@ return {
         end,
       },
     },
+    keys = {
+      {
+        "<leader>fE",
+        function()
+          require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
+          --require("neo-tree.command").execute({ toggle = true, dir = LazyVim.root() })
+        end,
+        desc = "Explorer NeoTree (Root Dir)",
+      },
+      { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (Root Dir)", remap = true },
+      {
+        "<leader>fe",
+        function()
+          require("neo-tree.command").execute({ toggle = true, reveal = true, position = "current" })
+        end,
+        desc = "Explorer NeoTree (Root Dir)",
+      },
+      { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (cwd)", remap = true },
+    },
   },
 }
