865743295647c5269b319ef4207f1975eac76330
wanglei <wllenyj@gmail.com>
2024-08-15 00:16:45 +0800

    nvim: format cmp.lua
    Signed-off-by: wanglei <wllenyj@gmail.com>

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,33 +1,32 @@
 return {
 
-  {
-    "nvim-cmp",
-    lazy = true,
-    --event = "InsertEnter",
-    --keys = "<leader>cp",
-    dependencies = {
-      {
-        "sourcegraph/sg.nvim",
+    {
+        "nvim-cmp",
+        event = "InsertEnter",
+        --keys = "<leader>cp",
         dependencies = {
-          "nvim-lua/plenary.nvim", --[[ "nvim-telescope/telescope.nvim ]]
+            {
+                "sourcegraph/sg.nvim",
+                dependencies = {
+                    "nvim-lua/plenary.nvim", --[[ "nvim-telescope/telescope.nvim ]]
+                },
+            },
         },
-      },
+        opts = function(_, opts)
+            require("sg").setup(opts)
+            table.insert(opts.sources, 1, {
+                name = "cody",
+                group_index = 1,
+                priority = 101
+            })
+        end,
     },
-    opts = function(_, opts)
-      require("sg").setup(opts)
-      table.insert(opts.sources, 1, {
-        name = "cody",
-        group_index = 1,
-        priority = 101
-    })
-    end,
-  },
-  --{
-  --  "nvim-lualine/lualine.nvim",
-  --  optional = true,
-  --  event = "VeryLazy",
-  --  opts = function(_, opts)
-  --    table.insert(opts.sections.lualine_x, 2, LazyVim.lualine.cmp_source("cody"))
-  --  end,
-  --},
+    --{
+    --  "nvim-lualine/lualine.nvim",
+    --  optional = true,
+    --  event = "VeryLazy",
+    --  opts = function(_, opts)
+    --    table.insert(opts.sections.lualine_x, 2, LazyVim.lualine.cmp_source("cody"))
+    --  end,
+    --},
 }
