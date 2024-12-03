ddd6dacaa2bd93e4c2b73b9c705defae8d10739e
wanglei <wllenyj@gmail.com>
2024-12-03 11:28:59 +0800

    nvim: remove some plugins
    Signed-off-by: wanglei <wllenyj@gmail.com>

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -12,8 +12,6 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
-    { import = "lazyvim.plugins.extras.coding.codeium" },
-    { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.coding.luasnip" },
     { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.lang.rust" },
