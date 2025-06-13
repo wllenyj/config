e50b63a26c621f096abc38f91a0229cc607bbc6b
wanglei <wllenyj@gmail.com>
2025-06-13 16:00:13 +0800

    nvim: modify the fzf plugin config
    Signed-off-by: wanglei <wllenyj@gmail.com>

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -1,32 +1,28 @@
 return {
   {
     "ibhagwan/fzf-lua",
-    opts = function(_, opts)
-      local actions = require("fzf-lua.actions")
-      opts["files"]["actions"] = nil
-      opts["grep"]["actions"] = nil
-      return {
-        keymap = {
-          builtin = {
-            ["<C-e>"] = "preview-down",
-            ["<C-y>"] = "preview-up",
-            ["<C-f>"] = "preview-page-down",
-            ["<C-b>"] = "preview-page-up",
-          },
+    opts = {
+      keymap = {
+        builtin = {
+          true,
+          ["<C-e>"] = "preview-down",
+          ["<C-y>"] = "preview-up",
+          ["<C-f>"] = "preview-page-down",
+          ["<C-b>"] = "preview-page-up",
         },
-        files = {
-          actions = {
-            ["ctrl-i"] = { actions.toggle_ignore },
-            ["ctrl-h"] = { actions.toggle_hidden },
-          },
-        },
-        grep = {
-          actions = {
-            ["ctrl-i"] = { actions.toggle_ignore },
-            ["ctrl-h"] = { actions.toggle_hidden },
-          },
-        },
-      }
-    end,
+      },
+      --files = {
+      --  actions = {
+      --    ["ctrl-i"] = { actions.toggle_ignore },
+      --    ["ctrl-h"] = { actions.toggle_hidden },
+      --  },
+      --},
+      --grep = {
+      --  actions = {
+      --    ["ctrl-i"] = { actions.toggle_ignore },
+      --    ["ctrl-h"] = { actions.toggle_hidden },
+      --  },
+      --},
+    },
   },
 }
