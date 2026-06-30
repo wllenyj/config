c2488fbc00b07652752a688f452515c44233f02d
wanglei <wllenyj@gmail.com>
2026-06-30 23:36:55 +0800

    format install.sh
    Signed-off-by: wanglei <wllenyj@gmail.com>

--- install.sh
+++ install.sh
@@ -8,9 +8,8 @@ HERE=$(readlink -f $0)
 CUR_PATH=$(cd $(dirname $HERE) && pwd)
 ((DEBUG)) && echo $CUR_PATH
 
-
-if ! command -v cargo &> /dev/null; then
-    if ! command -v curl &> /dev/null; then
+if ! command -v cargo &>/dev/null; then
+    if ! command -v curl &>/dev/null; then
         echo "❌ 未检测到 curl 命令，请先安装 curl"
         exit 1
     else
@@ -19,12 +18,11 @@ if ! command -v cargo &> /dev/null; then
 
         curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-	source $HOME/.bashrc
+        source $HOME/.bashrc
     fi
 fi
 
-
-if ! command -v zellij &> /dev/null; then
+if ! command -v zellij &>/dev/null; then
     echo "----------------------------------------"
     echo "✅ 开始安装 zellij..."
     cargo install --locked zellij || exit 1
@@ -36,22 +34,22 @@ else
     echo 'exist .config/zellij'
 fi
 
-if ! command -v rg &> /dev/null; then
+if ! command -v rg &>/dev/null; then
     echo "----------------------------------------"
     echo "✅ 开始安装 ripgrep..."
     cargo install ripgrep || exit 1
 fi
 
-if ! command -v atuin &> /dev/null; then
+if ! command -v atuin &>/dev/null; then
     echo "----------------------------------------"
     echo "✅ 开始安装 atuin..."
     cargo install atuin || exit 1
 fi
 
-if ! command -v fish &> /dev/null; then
+if ! command -v fish &>/dev/null; then
     echo "----------------------------------------"
     echo "✅ 开始安装 fish..."
-    if ! command -v apt-add-repository &> /dev/null; then
+    if ! command -v apt-add-repository &>/dev/null; then
         sudo apt update && sudo apt install -y software-properties-common || exit 1
     fi
     sudo apt-add-repository -y ppa:fish-shell/release-4 || exit 1
@@ -68,16 +66,15 @@ if [[ "$(getent passwd $USER | cut -d: -f7)" != "$FISH_PATH" ]]; then
     chsh -s "$FISH_PATH"
 fi
 
-
 grep "bash/prompt.sh" $HOME/.bashrc >>/dev/null
 if [[ $? != 0 ]]; then
-	echo -e "\nsource $CUR_PATH/bash/prompt.sh" >>$HOME/.bashrc
+    echo -e "\nsource $CUR_PATH/bash/prompt.sh" >>$HOME/.bashrc
 fi
 
 source $HOME/.bashrc
 
 if [[ ! -d $HOME/.vim ]]; then
-	ln -s $CUR_PATH/vim $HOME/.vim
+    ln -s $CUR_PATH/vim $HOME/.vim
 fi
 
 #if [[ ! -d $HOME/.screenrc ]]; then
@@ -85,27 +82,26 @@ fi
 #fi
 
 if [[ ! -d $HOME/.config/fish ]]; then
-	ln -s $CUR_PATH/fish $HOME/.config/fish
+    ln -s $CUR_PATH/fish $HOME/.config/fish
 else
-	echo 'exist .config/fish'
+    echo 'exist .config/fish'
 fi
 
 if [[ ! -d $HOME/.config/tmux ]]; then
-	ln -s $CUR_PATH/tmux $HOME/.config/tmux
+    ln -s $CUR_PATH/tmux $HOME/.config/tmux
 else
-	echo 'exist .config/tmux dir'
+    echo 'exist .config/tmux dir'
 fi
 
 if [[ ! -d $HOME/.config/nvim ]]; then
-	#mkdir $HOME/.config/nvim
-	ln -s $CUR_PATH/nvim $HOME/.config/nvim
+    #mkdir $HOME/.config/nvim
+    ln -s $CUR_PATH/nvim $HOME/.config/nvim
 else
-	echo 'exist .config/nvim'
+    echo 'exist .config/nvim'
 fi
 
 if [[ ! -d $HOME/.config/gitui ]]; then
-	ln -s $CUR_PATH/gitui $HOME/.config/gitui
+    ln -s $CUR_PATH/gitui $HOME/.config/gitui
 else
-	echo 'exist .config/gitui'
+    echo 'exist .config/gitui'
 fi
-
